3adb5f2083df0e27cee40a769676da39b67fe85b
MEKARI <mekari@MEKARIs-MacBook-Pro-5.local>
2024-12-24 19:32:53 +0700

    chore: init .vscode/settings.json

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,3 @@
 <template>
-  <h1 class="text-3xl font-bold underline text-red-600">
-    Hello world!
-  </h1>
+  <h1 class="text-3xl font-bold underline text-blue-600">Hello world!</h1>
 </template>
